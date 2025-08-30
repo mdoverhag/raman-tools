da66b116f78edc200b5d5fc68e53589ccc9d00ce
Mikael Doverhag <doverhag@gmail.com>
2025-08-30 13:20:37 +1000

    Cleaned up base tauri-app example app content

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,12 +2,6 @@ mod spectrum;
 
 use spectrum::Spectrum;
 
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 #[tauri::command]
 fn parse_spectrum_files(filepaths: Vec<String>) -> Result<Vec<Spectrum>, String> {
     spectrum::parse_files(filepaths)
@@ -17,7 +11,7 @@ fn parse_spectrum_files(filepaths: Vec<String>) -> Result<Vec<Spectrum>, String>
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet, parse_spectrum_files])
+        .invoke_handler(tauri::generate_handler![parse_spectrum_files])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
